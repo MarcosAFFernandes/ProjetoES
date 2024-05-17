c366d5d4aa286aa1516c56e250c3f3e296617f1b
RodrigoPestana35 <2079821@student.uma.pt>
2024-05-17 20:08:20 +0100

    Singleton-pestana

--- ProjetoES/ProjetoES/Models/Forum.cs
+++ ProjetoES/ProjetoES/Models/Forum.cs
@@ -3,10 +3,20 @@
     public class Forum
     {
 
+        private static Forum _instance;
         public List<Post> Posts { get; set; }
         public Forum()
         {
             Posts = new List<Post>();
+        }
+
+        public static Forum GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Forum();
+            }
+            return _instance;
         }
         public void AddPost(Post post)
         {
